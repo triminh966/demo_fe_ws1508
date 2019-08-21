639be887151a08c2e9b9dec83263594b12f5ba14
Tri TRAN <tminhtri@orangetheoryfitness.com>
2019-08-21 10:11:54 +0700

    add source

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Customers from './Customers'
+import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+
+const client = new W3CWebSocket('wss://nno8ermtec.execute-api.us-east-1.amazonaws.com/dev');
+
+class App extends Component {
+
+  componentWillMount() {
+    client.onopen = () => {
+      console.log('WebSocket Client Connected');
+    };
+    client.onmessage = (message) => {
+      console.log(message);
+    };
+  }
+
+  render() {
+    console.log("Host URL"+process.env.PUBLIC_URL);
+    return (
+
+      <Router basename={process.env.PUBLIC_URL}>
+        <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Simple React App</h1>
+        </header>
+          <Switch>
+                <Route exact path= "/" render={() => (
+                  <Redirect to="/customerlist"/>
+                )}/>
+                 <Route exact path='/customerlist' component={Customers} />
+          </Switch>
+      </div>
+    </Router>
+    );
+  }
+}
+
+export default App;
